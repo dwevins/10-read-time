b77af90645875b87d33f1580037e2d3390dbee0d
Danny Evins <dcastkid@gmail.com>
2016-06-29 16:54:01 -0500

    readTime works now

--- src/read-time.js
+++ src/read-time.js
@@ -1,1 +1,10 @@
 'use strict';
+
+function readTime(inputString, wpm = 1) {
+  let time = 0;
+  const rate = wpm;
+  const totalWords = stringToWords(inputString).length;
+  time = Math.ceil(totalWords / rate);
+
+  return time;
+}
