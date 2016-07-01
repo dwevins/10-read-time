28098d9d1fe385b92c44a6bdca313cf0e6871349
Danny Evins <dcastkid@gmail.com>
2016-07-01 11:35:55 -0500

    finish JS tests, format navbar in CSS

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 'use strict';
 /* globals numWords, readTime */
 
-const allBlogs = document.querySelectorAll(`.entry`);
-let currBlog = ``;
-for (let i = 0; i < allBlogs.length; i++) {
-  currBlog = allBlogs[i].querySelector(`.entry__content`);
-  console.log(allBlogs.length);
-  console.log(currBlog);
+const bodyElement = document.querySelector(`body`);
+const nav = document.createElement(`nav`);
+nav.className = `navbar`;
+bodyElement.insertBefore(nav, bodyElement.firstChild);
+const blogEntries = document.querySelectorAll(`.entry`);
+const entryTitle = document.querySelectorAll(`.entry__title`);
+const entryInfo = document.querySelectorAll(`.entry__info`);
+const entryContent = document.querySelectorAll(`.entry__content`);
+
+for (let i = 0; i < blogEntries.length; i++) {
+  const timeLi = document.createElement(`li`);
+  timeLi.className = `read-time`;
+  entryInfo[i].appendChild(timeLi);
+  timeLi.innerText = `${readTime(entryContent[i].innerText, 200)} mins`;
+
+  const navLink = document.createElement(`a`);
+  navLink.className = `navbar__link`;
+  navLink.setAttribute(`href`, `#${blogEntries[i].id}`);
+  navLink.innerText = entryTitle[i].innerText;
+  nav.appendChild(navLink);
 }
