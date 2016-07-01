66c11ec107c96cfdcbc7a82589e5161c2c5aee23
Danny Evins <dcastkid@gmail.com>
2016-06-30 19:36:48 -0500

    attempting last test

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 'use strict';
 /* globals numWords, readTime */
 
-const allBlogs = document.querySelectorAll(`.blog`);
-
-for (var i = 0; i < allBlogs.length; i++) {
-  allBlogs[i];
+const allBlogs = document.querySelectorAll(`.entry`);
+let currBlog = ``;
+for (let i = 0; i < allBlogs.length; i++) {
+  currBlog = allBlogs[i].querySelector(`.entry__content`);
+  console.log(allBlogs.length);
+  console.log(currBlog);
 }
